9015eee42d30978ae5d34230583c468fa63e0721
GreenSpleenMan <cruz.spirdonoff@nazareth.catholic.edu.au>
2025-12-02 10:22:54 +1030

    Create waves.js

---
+++ js/waves.js
@@ -0,0 +1,12 @@
+export const WAVES = [
+    { circle: 5 },
+    { circle: 10, triangle: 1 },
+    { circle: 7, triangle: 3, square: 1 },
+    { circle: 10, triangle: 5, square: 2 },
+    { circle: 2, triangle: 10, square: 5 },
+    { circle: 5, triangle: 5, square: 5 },
+    { circle: 1, triangle: 2, square: 3, octagon: 1 },
+    { circle: 10, triangle: 5, square: 3, octagon: 3 },
+    { pentagon: 1 },
+    { circle: 5, triangle: 5, square: 5, octagon: 5, pentagon: 5, hexagon: 1 }
+];
